dd1ac0756904a9d094c3001f2d411be94954e24f
wangdafull <47555545+jiaoda83@users.noreply.github.com>
2025-08-09 17:29:23 +0800

    Update client.rs

--- src/client.rs
+++ src/client.rs
@@ -737,8 +737,8 @@ impl Client {
 
             if !key.is_empty() && !token.is_empty() {
                 // mainly for the security of token
-                // secure_tcp(&mut socket, key).await?;
-                allow_err!(secure_tcp(&mut socket, key).await);
+                secure_tcp(&mut socket, key).await?;
+                // allow_err!(secure_tcp(&mut socket, key).await);
             }
 
             ipv4 = socket.local_addr().is_ipv4();
